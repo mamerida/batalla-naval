5f34b69ff839d67859574fa0ffce5868a06a08f1
Mario <mamerida2013@gmail.com>
2022-04-16 17:06:20 -0300

    piezas tablero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 
-import style from './style.css';
+import './style.css';
 import { useState } from 'react';
 
 function App() {
 
   const [partida, setPartida] = useState(false);
+
+  const piezas = [
+    {
+      nombre:"barco",
+      largo:[0]
+    },
+    {
+      nombre:"buque",
+      largo:[0,1]
+    },
+    {
+      nombre:"submarino",
+      largo:[0,1,2]
+    },
+    {
+      nombre:"portaAviones",
+      largo:[0,1,2,3]
+    }
+  ]
 
   const comenzarPartida = () => {
     setPartida(true);
@@ -15,9 +34,13 @@ function App() {
       <h3>Batalla Naval</h3>
       <div className='contenedor'>
         <button className='botonStart' onClick={comenzarPartida}>Comenzar Juego</button>
-        <div className='TableroJuego'>
+        <div className={partida ? "TableroJuego" : "none"}>
+          <div className='ContenedorTablero'>
           <div className='tableroJugador'></div>
           <div className='tableroPc'></div>
+          </div>
+          <label>Coloca tus piezas</label>
+          {piezas.map(pieza => <li key={pieza.nombre}  draggable="true">{pieza.nombre}</li>)}
         </div>
       </div>
     </div>
