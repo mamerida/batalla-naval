a1749e871ebf2a8fb8c44308e59b536688bcabc1
Mario <mamerida2013@gmail.com>
2022-04-16 18:01:07 -0300

    creador y selector de piezas

--- src/App.js
+++ src/App.js
@@ -7,29 +7,44 @@ function App() {
   const [partida, setPartida] = useState(false);
   const [tableroJugador, setTableroJugador] = useState([]);
   const [tableroPc, setTableroPC] = useState([]);
+
+
+  const ColocarPieza = ({pieza}) => {
+
+    return (
+      <>
+          <div>{pieza.nombre}</div>
+        {pieza.largo.map((casilla)=>{         
+          return <div key={casilla.value} className={` casillaJugador ${pieza.nombre}`}></div>
+        })}
+      </>
+    )
+  }
+
+
   const piezas = [
     {
-      nombre:"barco",
-      largo:[0]
+      nombre: "barco",
+      largo: [0]
     },
     {
-      nombre:"buque",
-      largo:[0,1]
+      nombre: "buque",
+      largo: [0, 1]
     },
     {
-      nombre:"submarino",
-      largo:[0,1,2]
+      nombre: "submarino",
+      largo: [0, 1, 2]
     },
     {
-      nombre:"portaAviones",
-      largo:[0,1,2,3]
+      nombre: "portaAviones",
+      largo: [0, 1, 2, 3]
     }
   ]
 
   const comenzarPartida = () => {
     setPartida(true);
-    setTableroJugador([["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""]])
-    setTableroPC([["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""]])
+    setTableroJugador([["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""]])
+    setTableroPC([["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""]])
   }
 
   return (
@@ -42,6 +57,9 @@ function App() {
 
           </div>
           <label>Coloca tus piezas</label>
+          {piezas.map(pieza => {
+            return <ColocarPieza pieza={pieza} />
+          })}
           <div className='inputsContainer'>
 
           </div>
